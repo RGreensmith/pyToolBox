58892a833738d28068c950ff4b8ba59fbd61d5b5
RGreensmith <rose.greensmith@yahoo.co.uk>
2021-01-07 12:10:10 +0000

    Initial structure of scaling function

--- scaling.py
+++ scaling.py
@@ -1,4 +1,5 @@
-def scaling () :
+import numpy as np
+def scaling (data, a = 0, b = 1, columnIndices = 0) :
 
     """
     Scales data between a user specified range.
@@ -8,3 +9,14 @@ def scaling () :
     Returns:
 
     """
+    dataScaled = data
+    for i in range(len(columnIndices)) :
+        minX = min(data[:, columnIndices[i]])
+        maxX = max(data[:, columnIndices[i]])
+        for j in range(len(data[:,columnIndices])) :
+            div = (data[j, i] - minX)/(maxX-minX)
+            dataScaled[j, i] = ((b-a)*div)+a
+        cor = np.corrcoeff(dataScaled[:, columnIndices[i]], data[:, columnIndices[i]])
+        print('Correlation coefficient between original and scaled data (',+columnIndices[i],') = ',+cor)
+    
+
